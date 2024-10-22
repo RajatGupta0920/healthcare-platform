e5b42cda12cf140ea2dbd1145f5d0a9ff22db326
Rajat-pixel-codes <65059677+Rajat-pixel-codes@users.noreply.github.com>
2024-10-22 13:07:27 +0530

    third commit

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -18,35 +18,45 @@ export const columns: ColumnDef<Appointment>[] = [
   {
     accessorKey: 'patient',
     header: 'Patient',
-    cell: ({ row }) => (
-      <p className="text-14-medium">{row.original.patient.name}</p>
-    ),
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return <p className="text-14-medium ">{appointment.patient.name}</p>;
+    },
   },
   {
     accessorKey: 'status',
     header: 'Status',
-    cell: ({ row }) => (
-      <div className="min-w-[115px]">
-        <StatusBadge status={row.original.status} />
-      </div>
-    ),
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <div className="min-w-[115px]">
+          <StatusBadge status={appointment.status} />
+        </div>
+      );
+    },
   },
   {
     accessorKey: 'schedule',
     header: 'Appointment',
-    cell: ({ row }) => (
-      <p className="text-14-regular min-w-[100px]">
-        {formatDateTime(row.original.schedule).dateTime}
-      </p>
-    ),
+    cell: ({ row }) => {
+      const appointment = row.original;
+      return (
+        <p className="text-14-regular min-w-[100px]">
+          {formatDateTime(appointment.schedule).dateTime}
+        </p>
+      );
+    },
   },
   {
     accessorKey: 'primaryPhysician',
-    header: () => 'Doctor',
+    header: 'Doctor',
     cell: ({ row }) => {
+      const appointment = row.original;
+
       const doctor = Doctors.find(
-        (doc) => doc.name === row.original.primaryPhysician
+        (doctor) => doctor.name === appointment.primaryPhysician
       );
+
       return (
         <div className="flex items-center gap-3">
           <Image
